862641fb64a9cef090395c50b284ef537adb4ff9
Jan Kevin Dick <Jan.Dick@de.bosch.com>
2024-01-24 19:05:28 +0100

    Fixed error

--- consumer/src/consumer.cpp
+++ consumer/src/consumer.cpp
@@ -1,7 +1,5 @@
 #include "consumer/consumer.h"
 
-#include <mylib/mylib_impl.h>
-
 #include <iostream>
 #include <sstream>
 
@@ -9,8 +7,6 @@ Consumer::Consumer(std::unique_ptr<MyLib> mylib) : myLib_{std::move(mylib)} {
 }
 
 std::string Consumer::data() const {
-    MyLibImpl impl;
-    std::cout << impl.name() << '\n';
     std::stringstream ss;
     ss << "MyLib Name: ";
     ss << myLib_->name();
